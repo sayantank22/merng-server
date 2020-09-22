18040cb943acdffd1a5848450c2bc8aba8a7a142
Sayantan Karmakar <sayantank22@gmail.com>
2020-09-23 03:27:31 +0530

    solving deployment issue

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB_URI:
+    'mongodb://sayantank22:Student%4013@devconnector-shard-00-00.txr6s.mongodb.net:27017,devconnector-shard-00-01.txr6s.mongodb.net:27017,devconnector-shard-00-02.txr6s.mongodb.net:27017/MERNG?ssl=true&replicaSet=atlas-b7ig4u-shard-0&authSource=admin&retryWrites=true&w=majority',
+  SECRET_KEY: 'merng-secret-key',
+};
